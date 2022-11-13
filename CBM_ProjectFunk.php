b9e50ede3e861cdb29d299777470fa8e278398d2
Michael <112928771+ze1234ze@users.noreply.github.com>
2022-11-13 17:40:57 +0100

    Update CBM_ProjectFunk.php

--- CBM_ProjectFunk.php
+++ CBM_ProjectFunk.php
@@ -7,14 +7,8 @@ $budget = $_POST["budget"];
 $result = 0;
 
 
-
-
-
-
 $search = "SELECT * FROM cbm_project WHERE  preis <= $budget && age <= $age ORDER BY RAND()
 LIMIT 1";
-
-// random_between(1,20)
 
 
 try {
@@ -24,38 +18,16 @@ try {
         ""           // password
     );
 
-    // echo "gut";
-
-
-
+   
     $result = $data_bank->query($search);
     $antwort = $result->fetchAll(PDO::FETCH_ASSOC);
-
     $data_bank = null;  // macht variabel frei
 
 
     foreach ($antwort as $line) {
-
-
-
-        // echo "The Name: "  . $line["name"] . "<br>";
-        // echo "Describtion: "  . $line["describe"] . "<br>";
-        // echo "Adress: "  . $line["adress"] . "<br>";
-        // echo "Kontakt: "  . $line["kontakt"] . "<br>";
-
-
     }
+    
 } catch (PDOException $search) {
     echo $search->getMessage();
 }
 
-
-
-
-
-// $test = "CREATE TABLE bestellungen" . 
-//              "(id integer unsigned not null auto_increment PRIMARY KEY," . 
-//              "artikelnummer integer unsigned not null,". 
-//              "kundennummer integer unsigned not null," . 
-//              "preis decimal(7,2) unsigned not null," . 
-//              "anzahl integer unsigned);";
